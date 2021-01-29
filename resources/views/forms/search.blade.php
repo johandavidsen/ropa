a72b09b2fc5531c60d75c2c5ff538488911c2b26
Jóhan Dam-Davidsen <johan.davidsen@fjakkarin.com>
2021-01-29 14:30:13 -0300

    Added font files and updated the nav

--- resources/views/forms/search.blade.php
+++ resources/views/forms/search.blade.php
@@ -1,17 +1,16 @@
-<form role="search" method="get" class="" action="{{ home_url('/') }}">
-  <label>
+<form role="search" method="get" class="border" action="{{ home_url('/') }}">
+  {{--<label>
     <span class="screen-reader-text">
       {{ _x('Search for:', 'label', 'sage') }}
     </span>
+  </label>--}}
+  <input
+    type="search"
+    class="search-field pl-2 p-1"
+    placeholder="{!! esc_attr_x('Search &hellip;', 'placeholder', 'sage') !!}"
+    value="{{ get_search_query() }}"
+    name="s"
+  >
 
-    <input
-      type="search"
-      class="search-field"
-      placeholder="{!! esc_attr_x('Search &hellip;', 'placeholder', 'sage') !!}"
-      value="{{ get_search_query() }}"
-      name="s"
-    >
-  </label>
-
-  <input type="submit" class="button" value="{{ esc_attr_x('Search', 'submit button', 'sage') }}">
+  <input type="submit" class="button p-1 pr-2" value="{{ esc_attr_x('Search', 'submit button', 'sage') }}">
 </form>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,16 @@
 <header>
-  <div class="nav">
+  <div class="nav p-4">
     <a href="{{ home_url('/') }}" class="flex items-center text-lg font-pacifico flex-1">
       {{ $siteName }}
     </a>
 
     <nav class="flex-1">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'flex justify-center', 'echo' => false]) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'flex justify-center', 'echo' => false ]) !!}
       @endif
     </nav>
 
-    <div class="flex-1 text-right">
+    <div class="flex-1 flex justify-end">
       @include('forms/search')
     </div>
   </div>
