f49da109f15a0bddfa0025ff54d72c1020fbb46c
Jóhan H Dam-Davidsen <johan.davidsen@fjakkarin.com>
2022-01-03 13:16:59 -0300

    Removed unused blocks

--- app/blocks.php
+++ app/blocks.php
@@ -1,21 +1,8 @@
 <?php
 
-use App\View\Blocks\PostWidget;
-
 /**
  * Blocks
  *
  * This file registers all the blocks required for this theme.
  *
  */
-
-if (! function_exists('ropa_register_blocks')) {
-
-
-    function ropa_register_blocks()
-    {
-        //$postWidget = new PostWidget();
-        //$postWidget->bootstrap();
-    }
-    add_action('init', 'ropa_register_blocks');
-}
--- resources/views/blocks/posts-widget.blade.php
+++
@@ -1,1 +0,0 @@
-<div>hello 2</div>
