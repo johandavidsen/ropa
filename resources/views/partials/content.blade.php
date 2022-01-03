b9b134780fc20b6023f84d67f2e22b88e731f6f3
Jóhan H Dam-Davidsen <johan.davidsen@fjakkarin.com>
2022-01-03 14:20:01 -0300

    Updated style

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -5,7 +5,7 @@
     <div class="flex-1 bg-white p-6 flex flex-col justify-between">
         <div class="flex-1">
             <a href="{!! get_permalink() !!}" class="block mt-2">
-                <h2 class="text-xl font-semibold text-gray-900">
+                <h2 class="">
                     {!! $title !!}
                 </h2>
                 <p class="mt-3 text-base text-gray-500">
